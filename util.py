27e2f8b60649e1734fc0fe223e7e3b7258f95c03
Ekke-kuru2 <71656810+Ekke-kuru2@users.noreply.github.com>
2022-07-10 01:09:02 +0900

    selfを引数に入れ忘れてた

--- util.py
+++ util.py
@@ -8,7 +8,7 @@ class util():
         return pd.Series(candles["close"]).rolling(window = span).mean()
 
     @staticmethod
-    def cal_amount(exchange:ccxt.bybit):
+    def cal_amount(self,exchange:ccxt.bybit):
         ticker_info = exchange.fetch_ticker(config.symbol)
         ticker_balance = exchange.fetch_balance()
         return ticker_balance["free"]["USDT"]*config.leverage/ticker_info["close"]*2
