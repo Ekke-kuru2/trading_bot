81a20bd28800e38bb20278fb15438e7a694cea31
Ekke-kuru2 <71656810+Ekke-kuru2@users.noreply.github.com>
2022-07-10 02:14:52 +0900

    メインロジックを感覚で完成

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import time
 import ccxt
 import json
 import pandas as pd
-#import matplotlib as mp
-import sys,os
+import matplotlib as mp
+import os
 
 from slack import slack_webhook
 from util import util
@@ -18,35 +18,109 @@ exchange.set_sandbox_mode(True)
 
 print(util.cal_amount(exchange))
 
-#slack_webhook("test")
+slack_webhook("START TRADING")
 
+position = 0 # 0:None 1:buy 2:sell
+amount = 0
+rate_at=0
 
 while True:
     # every 15minutes
     minute=0
 
     since_time=int(time.time()-60*15*60)*1000 # 現在のUNIX秒から60足分戻してmsに
+    
+    #ロウソク足を取得
     rate_df = pd.DataFrame(data=exchange.fetch_ohlcv(symbol=config.symbol,timeframe=config.timeframe,since=since_time,limit=60),columns=["timestamp","open","high","low","close","volume"])
 
-    sma_5 = util.make_sma(rate_df, 30) # 短期移動平均線を作成 ※期間についてはお好み
-    sma_13 = util.make_sma(rate_df, 60) # 長期移動平均線を作成 ※期間についてはお好み
+    sma_short = util.make_sma(rate_df, 30) # 短期移動平均線を作成 ※期間についてはお好み
+    sma_long = util.make_sma(rate_df, 60) # 長期移動平均線を作成 ※期間についてはお好み
 
-    # 短期移動平均線 > 長期移動平均線 の状態が3本続いたらゴールデンクロス（騙し防止のために判断まで少し待つ）
-    golden_cross = sma_5.iloc[-1] > sma_13.iloc[-1] \
-        and sma_5.iloc[-2] > sma_13.iloc[-2] \
-        and sma_5.iloc[-3] > sma_13.iloc[-3] \
-        and sma_5.iloc[-4] < sma_13.iloc[-4]
+    # 短期移動平均線 < 長期移動平均線 が 短期移動平均線 > 長期移動平均線 になったらゴールデンクロス
+    golden_cross = sma_short.iloc[-1] > sma_long.iloc[-1] \
+        and sma_short.iloc[-2] < sma_long.iloc[-2] 
 
-    # 短期移動平均線 < 長期移動平均線 の状態が3本続いたらデッドクロス（騙し防止のために判断まで少し待つ）
-    dead_cross = sma_5.iloc[-1] < sma_13.iloc[-1] \
-        and sma_5.iloc[-2] < sma_13.iloc[-2] \
-        and sma_5.iloc[-3] < sma_13.iloc[-3] \
-        and sma_5.iloc[-4] > sma_13.iloc[-4]
+    # 短期移動平均線 > 長期移動平均線 が 短期移動平均線 < 長期移動平均線 になったらデッドクロス
+    dead_cross = sma_short.iloc[-1] < sma_long.iloc[-1] \
+        and sma_short.iloc[-2] > sma_long.iloc[-2] 
 
-    print(golden_cross,dead_cross)
+
+    if position!=0: # ポジションを持ってないとき
+        if golden_cross:
+            amount = util.cal_amount(exchange)
+            balance = util.get_balance_now(exchange)
+
+            exchange.create_order(symbol=config.symbol, type='market', side='buy', amount=amount)
+            rate_at = util.get_rate_now(exchange)
+            position = 1
+            print("[BUY] Captured golden cross! \n symbol: "+config.symbol+"\n amount: "+str(amount)+"\n rate: "+str(rate_at)+"\n  balance now: "+str(balance))
+            slack_webhook("[BUY] Captured golden cross! \n symbol: "+config.symbol+"\n amount: "+str(amount)+"\n rate: "+str(rate_at)+"\n balance now: "+str(balance))
+        elif dead_cross:
+            amount = util.cal_amount(exchange)
+            balance = util.get_balance_now(exchange)
+
+            exchange.create_order(symbol=config.symbol, type='market', side='sell', amount=amount)
+            rate_at = util.get_rate_now(exchange)
+            position = 2
+            print("[SELL] Captured dead cross! \n symbol: "+config.symbol+"\n amount: "+str(amount)+"\n rate: "+str(rate_at)+"\n balance now: "+str(balance))
+            slack_webhook("[SELL] Captured dead cross! \n symbol: "+config.symbol+"\n amount: "+str(amount)+"\n rate: "+str(rate_at)+"\n balance now: "+str(balance))
 
     while minute<15:
         # every minute
+
+        if position == 1: # 買ってたら
+            rate_now = util.get_rate_now(exchange)
+            change_rate = (rate_at/rate_now)*100
+            if change_rate>102: # 2%以上の値上がりで利確
+                exchange.create_order(symbol=config.symbol, type='market', side='sell', amount=amount,params={"reduceOnly":True})
+                position = 0
+                amount = 0
+                rate_at = 0
+                time.sleep(1)
+                balance = util.get_balance_now(exchange)
+
+                print("[PROFIT] Captured 2% UP \n balance now: "+str(balance))
+                slack_webhook("[PROFIT] Captured 2% UP \n balance now: "+str(balance))
+
+            elif change_rate<99: # 1%以上の値下がりで損切
+                exchange.create_order(symbol=config.symbol, type='market', side='sell', amount=amount,params={"reduceOnly":True})
+                position = 0
+                amount = 0
+                rate_at = 0
+
+                time.sleep(1)
+                balance = util.get_balance_now(exchange)
+
+                print("[LOSS] Captured 1% DOWN \n balance now: "+str(balance))
+                slack_webhook("[LOSS] Captured 1% DOWN \n balance now: "+str(balance))
+            
+        elif position ==2: # 売ってたら
+            rate_now = util.get_rate_now(exchange)
+            change_rate = (rate_at/rate_now)*100
+            if change_rate<98: # 2%以上の値下がりで利確
+                exchange.create_order(symbol=config.symbol, type='market', side='buy', amount=amount,params={"reduceOnly":True})
+                position = 0
+                amount = 0
+                rate_at = 0
+
+                time.sleep(1)
+                balance = util.get_balance_now(exchange)
+
+                print("[PROFIT] Captured 2% DOWN \n balance now: "+str(balance))
+                slack_webhook("[PROFIT] Captured 2% DOWN \n balance now: "+str(balance))
+
+            elif change_rate>101: # 1%以上の値上がりで損切
+                exchange.create_order(symbol=config.symbol, type='market', side='buy', amount=amount,params={"reduceOnly":True})
+                position = 0
+                amount = 0
+                rate_at = 0
+
+                time.sleep(1)
+                balance = util.get_balance_now(exchange)
+
+                print("[LOSS] Captured 1% UP \n balance now: "+str(balance))
+                slack_webhook("[LOSS] Captured 1% UP \n balance now: "+str(balance))
+
         time.sleep(60)
         minute+=1
         
--- util.py
+++ util.py
@@ -11,7 +11,16 @@ class util():
     def cal_amount(exchange:ccxt.bybit):
         ticker_info = exchange.fetch_ticker(config.symbol)
         ticker_balance = exchange.fetch_balance()
-        return ticker_balance["free"]["USDT"]*config.leverage/ticker_info["close"]/2
+        return ticker_balance["free"][config.currency]*config.leverage/ticker_info["close"]/2
+    
+    @staticmethod
+    def get_rate_now(exchange):
+        ticker_info = exchange.fetch_ticker(config.symbol)
+        return ticker_info["close"]
 
+    @staticmethod
+    def get_balance_now(exchange):
+        ticker_balance = exchange.fetch_balance()
+        return ticker_balance["free"][config.currency]
         
         
