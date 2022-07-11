254f6a7ed773772c03da689b6baf7bec3437f12f
Ekke-kuru2 <71656810+Ekke-kuru2@users.noreply.github.com>
2022-07-11 22:05:21 +0900

    bug fix

--- cross_Test.py
+++ cross_Test.py
@@ -26,20 +26,20 @@ while True:
 
     sma_short = util.make_sma(rate_df, 30) # 短期移動平均線を作成 ※期間についてはお好み
     sma_long = util.make_sma(rate_df, 60) # 長期移動平均線を作成 ※期間についてはお好み
-    #print(sma_short)
-    #print(sma_long)
+    print(sma_short)
+    print(sma_long)
 
-    '''
+    
     # 短期移動平均線 < 長期移動平均線 が 短期移動平均線 > 長期移動平均線 になったらゴールデンクロス
-    golden_cross = sma_short.iloc[-1] > sma_long.iloc[-1] 
+    golden_cross = sma_short.iloc[-1] > sma_long.iloc[-1] \
         and sma_short.iloc[-2] < sma_long.iloc[-2] 
 
     # 短期移動平均線 > 長期移動平均線 が 短期移動平均線 < 長期移動平均線 になったらデッドクロス
-    dead_cross = sma_short.iloc[-1] < sma_long.iloc[-1] 
+    dead_cross = sma_short.iloc[-1] < sma_long.iloc[-1] \
         and sma_short.iloc[-2] > sma_long.iloc[-2] 
     print("  golden cross: ",golden_cross,"\n  dead cross: ",dead_cross)
+    
     '''
-
     golden_cross = sma_short.iloc[-1] > sma_long.iloc[-1] \
             and sma_short.iloc[-2] > sma_long.iloc[-2] \
             and sma_short.iloc[-3] > sma_long.iloc[-3] \
@@ -50,6 +50,7 @@ while True:
         and sma_short.iloc[-2] < sma_long.iloc[-2] \
         and sma_short.iloc[-3] < sma_long.iloc[-3] \
         and sma_short.iloc[-4] > sma_long.iloc[-4]
+    '''
     print(golden_cross,dead_cross)
-    time.sleep(3)
+    time.sleep(1)
     i+=1
